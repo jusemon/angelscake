e7f56e9cbd567781d83c4bcd432b02069e043d30
Juan Sebastián Montoya <JuanSMM@outlook.com>
2019-11-19 15:46:05 -0500

    Added basic gameplay items

---
+++ Assets/Scripts/CollectableCake.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(AudioSource))]
+public class CollectableCake : MonoBehaviour
+{
+    public AudioClip collectableSound;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Check if the player collides with the angel's cake
+        if (collision.tag == "Player")
+        {
+            //TODO: to implement increase number of collected cakes
+
+            // Play the collectable sound
+            GetComponent<AudioSource>().PlayOneShot(collectableSound);
+
+            // Hide the cake
+            GetComponent<Renderer>().enabled = false;
+
+            // Destroy the cake after a delay
+            Destroy(gameObject, collectableSound.length);
+
+            // Destroy the script in case the player hits again the cake before that is destroyed
+            Destroy(this);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    private Rigidbody2D rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // FixedUpdate is called once per physic calculation
+    void FixedUpdate()
+    {
+        // Get new position of our character
+        var x = transform.position.x + Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+
+        var y = transform.position.y + Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        // Set new position
+        rigidBody.MovePosition(new Vector2(x, y));
+    }
+}
